b14c0ce7c2e9dc6c57333db5e4d7d65c837e0ea1
shaderka <73718120+shaderka@users.noreply.github.com>
2020-12-20 17:12:49 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,44 @@
+// (function() {
+// 	var cum = document.getElementById("dot");
+// 	var style = cum.style;
+// 	style.transform = "translateX(1700px)";
+// })();
+
+// function myFunction() {
+// 	var cum = document.getElementById("dot");
+// 	var cont = document.getElementsByClassName("dot");
+// 	for (let i=0; i<cont.length; i++) {
+// 		var stylei = cont[i].style;
+// 		if (i==1) {
+// 			stylei.opacity = "0";
+// 		}
+// 		if (i==2) {
+// 			stylei.opacity = "0";
+// 		}
+// 	}
+// 	var style = cum.style;
+// 	style.transform = "translateX(0px)";
+// 	style.opacity = "0";
+// }
+
+function myFunction() {
+	var x = document.getElementsByClassName("dot");
+	for (let i=0; i<x.length; i++) {
+		var style = x[i].style;
+	   if (i==0) {
+	   	style.transform = "translateX(1000px)";
+	   }
+	   if (i==1) {
+	   	style.transform = "translateX(-800px)";
+	   }
+	   if (i==2) {
+	   style.transform = "translateX(-1000px)";
+		}
+	}
+}
+
+
+
 document.addEventListener("mousemove" , parallax);
 function parallax(e){
 	this.querySelectorAll(".layer").forEach(layer => {
@@ -9,3 +50,4 @@ function parallax(e){
 		layer.style.transform = `translateX(${x}px) translateY(${y}px)`
 	})
 }
+
